f28d8b5f988c36b128216812ba5e6ed45f49c236
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-29 00:22:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var a=6;
-var h=7; 
-var triangleArea=a*h/2; 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var text= 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur= 'Triceratops';
+function getText(text, dinosaur) {
+var text2= text.replace('Velociraptor', dinosaur);
+return text2
+}
+var newText= getText(text, dinosaur);
+console.log(newText.length/2);
+console.log(newText.slice(0, 72));
