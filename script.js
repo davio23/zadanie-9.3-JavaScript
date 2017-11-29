36e45bf1ee69c857e9c237f62fd727096e0ff927
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-29 17:32:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-var text= 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur= 'Triceratops';
-function getText(text, dinosaur) {
-var text2= text.replace('Velociraptor', dinosaur);
-return text2
-}
-var newText= getText(text, dinosaur);
-console.log(newText.length/2);
-console.log(newText.slice(0, 72));
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.',
+    dinosaur = 'Triceratops',
+    newDinosaur = dinosaur.toUpperCase(),
+    text2= text.replace('Velociraptor', newDinosaur);
+console.log(text2.slice(0, text2.length / 2));
